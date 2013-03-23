36f10de08f917790f48975130d19869848e433bd
Romain Goyet <romain.goyet@applidium.com>
2013-03-23 20:02:58 +0100

    Foreground/background

--- top_app_store.js
+++ top_app_store.js
@@ -1,21 +1,20 @@
 $(function(){
   $.ajax({
-    url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml",
+    url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml",
     context: document.body
   }).done(function(data) {
     $.each($(data).find('entry'), function(index, value) {
-      addApplication(value);
+      addApplication(index <= 10 ? 'foreground' : 'background', value);
     });
   });
 });
 
-function addApplication(xmlNode) {
+function addApplication(kind, xmlNode) {
   var metadata = {
     "name": $(xmlNode).find('name').text(),
     "category": $(xmlNode).find('category').attr('label'),
     "icon": $(xmlNode).find('image').last().text(),
     "editor": $(xmlNode).find('artist').text()
   };
-  var appNode = Mustache.render($('#entry-template').html(), metadata);
-  $('#apps').append(appNode);
+  $('#apps-' + kind).append(Mustache.render($('#' + kind + '-app').html(), metadata));
 }
