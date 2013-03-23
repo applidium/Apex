fc657da88e4d6f6cc13f8ca88d3be3a5f1995638
Romain Goyet <romain.goyet@applidium.com>
2013-03-23 20:14:41 +0100

    Add the rank

--- top_app_store.js
+++ top_app_store.js
@@ -1,20 +1,22 @@
 $(function(){
   $.ajax({
-    url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml",
+    url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=100/xml",
     context: document.body
   }).done(function(data) {
     $.each($(data).find('entry'), function(index, value) {
-      addApplication(index <= 10 ? 'foreground' : 'background', value);
+      addApplication(index+1, index <= 10 ? 'foreground' : 'background', value);
     });
   });
 });
 
-function addApplication(kind, xmlNode) {
+function addApplication(index, kind, xmlNode) {
   var metadata = {
+    "rank": index,
     "name": $(xmlNode).find('name').text(),
     "category": $(xmlNode).find('category').attr('label'),
     "icon": $(xmlNode).find('image').last().text(),
     "editor": $(xmlNode).find('artist').text()
   };
+  console.log(metadata);
   $('#apps-' + kind).append(Mustache.render($('#' + kind + '-app').html(), metadata));
 }
