07d83b3ae2d7119e9cb450fa8ce781581a6b319e
Romain Goyet <romain.goyet@applidium.com>
2013-03-23 19:46:13 +0100

    Minor cleanup

--- top_app_store.js
+++ top_app_store.js
@@ -1,16 +1,11 @@
 $(function(){
-  console.log("Document loaded");
   $.ajax({
     url: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml",
     context: document.body
   }).done(function(data) {
-    console.log("Done fetching!");
-    console.log(data);
     $.each($(data).find('entry'), function(index, value) {
       addApplication(value);
     });
-    console.log($(data).find('entry'));
-    $(this).addClass("done");
   });
 });
 
@@ -22,5 +17,5 @@ function addApplication(xmlNode) {
     "editor": $(xmlNode).find('artist').text()
   };
   var appNode = Mustache.render($('#entry-template').html(), metadata);
-  $('#insertion').append(appNode);
+  $('#apps').append(appNode);
 }
